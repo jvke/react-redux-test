643744830ce993488c449cd1596219a6f78553dd
Jake <hi@jvke.co>
2016-10-21 09:37:48 +1100

    Added src/ and test/ dirs with example test files

---
+++ test/demo.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+
+// Enzyme examples:
+// http://codeheaven.io/testing-react-components-with-enzyme/
+
+describe('Example', () => {
+  it('works', () => {
+    expect(true).to.exist;
+  });
+});
